ccee7cd422e0975c6b57e80115ed69afaa5d27e2
Khushi Singh <ksingh.172005@gmail.com>
2025-08-16 19:37:29 +0530

    feat: code the token and tokentype with basic scanner

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 
 
 public class Lox {
+    static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -32,6 +33,7 @@ public class Lox {
         for (Token token :tokens){
             System.out.println(token);
         }
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -43,8 +45,41 @@ public class Lox {
             String line = reader.readLine();
             if(line == null) break ;
             run(line);
+            hadError = false ;
         }
     }
+
+    static void error(int line, String message ){
+        report(line ,"", message);
+
+    }
+    private static void report (int line, String where, String message){
+        System.err.println("[line " + line +"] Error "+where + ": "+ message);
+        hadError = true;
+    }
+
+    enum TokenType{
+        //Single-character tokens.
+        LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+        //One or two character tokens.
+        BANG, BANG_EQUAL,
+        EQUAL, EQUAL_EQUAL,
+        GREATER, GREATER_EQUAL, 
+        LESS, LESS_EQUAL,
+
+        //Literals.
+        IDENTIFIER, STRING, NUMBER,
+
+        //Keywords.
+        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NUL, OR,
+        PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+        EOF
+
+    }
 }
+
 
 
