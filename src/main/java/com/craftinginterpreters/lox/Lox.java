1688e75d5f14c0c230c47bae8daaff456320f279
Khushi Singh <ksingh.172005@gmail.com>
2025-08-16 22:35:03 +0530

    fix: delete the enum TokenType

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+
 
 
 public class Lox {
@@ -28,7 +30,7 @@ public class Lox {
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
-        List<Tokens> tokens = scanner.ScanTokens();
+        List<Token> tokens = scanner.scanTokens();
 
         for (Token token :tokens){
             System.out.println(token);
@@ -58,27 +60,7 @@ public class Lox {
         hadError = true;
     }
 
-    enum TokenType{
-        //Single-character tokens.
-        LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-
-        //One or two character tokens.
-        BANG, BANG_EQUAL,
-        EQUAL, EQUAL_EQUAL,
-        GREATER, GREATER_EQUAL, 
-        LESS, LESS_EQUAL,
-
-        //Literals.
-        IDENTIFIER, STRING, NUMBER,
-
-        //Keywords.
-        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NUL, OR,
-        PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-
-        EOF
-
-    }
+   
 }
 
 
