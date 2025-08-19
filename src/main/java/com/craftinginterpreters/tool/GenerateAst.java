8a82c72504fbb5eb9ce9e3271d92c71b499bd907
Khushi Singh <ksingh.172005@gmail.com>
2025-08-19 16:17:38 +0530

    feat: write a function to output the base class

--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -18,6 +18,22 @@ public class GenreateAst{
             "Literal : Object value",
             "Unary : Token operator , Expr right"
         ));
+
     }
+
+    private static void defineAst(
+        String outputDir, String baseName, List<String> types) throws IOException {
+            String path = outputDir + "/" + baseName + ".java";
+            PrintWriter writer = new PrintWriter(path," UTF-8");
+
+            writer.println("package com.craftinginterpreters.lox;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class " + baseName + " {");
+            writer.println("}");
+            writer.close();
+
+        }
 
 }
