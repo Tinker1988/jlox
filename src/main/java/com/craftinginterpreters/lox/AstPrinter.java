1a2b843792b96eef9381967b0fdde8bfa57eff60
Khushi Singh <ksingh.172005@gmail.com>
2025-08-21 03:38:47 +0530

    feat: add a ASTPrinter

---
+++ src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -0,0 +1,55 @@
+package com.craftinginterpreter.lox;
+
+
+
+import com.craftinginterpreters.lox.Expr;
+
+
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr){
+        return expr.accept(this);
+    }
+    @Override
+    public String visitBinaryExpr( Expr.Binary expr){
+        return parenthesize(expr.operator.lexeme, expr.left,expr.right);
+
+    }
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr){
+        return parenthezise("group", expr.expression);
+
+    }
+    @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return parenthesize(expr.operator.lexeme, expr.right);
+  }
+  private String parenthesize(String name, Expression...exprs){
+    StringBuilder builder = new StringBuilder();
+
+    builder.append("(").append(name);
+    for (Expr expr : exprs){
+        builder.append(" ");
+        builder.append(expr.accept(this));
+    }
+    builder.append(")");
+
+    return builder.toString();
+
+  }
+  public static void main(String[] args){
+    Expr expression = new Expr.Binary(
+        new Expression.Unary(
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(123)),
+            new Token(tokenType.STAR, "*", null, 1),
+            new Expression.Grouping(
+                new Expression.Literal(45.67)));
+    System.out.println(new AstPrinter().print(expression));
+  }
+}
