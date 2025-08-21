59498021ce198ec66a03ce63e5524b9184f867f1
Khushi Singh <ksingh.172005@gmail.com>
2025-08-21 14:30:43 +0530

    feat: start writing code for the parser

---
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+class Parser{
+    private final List<Token> tokens;
+
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = null;
+    }
+
+    
+}
