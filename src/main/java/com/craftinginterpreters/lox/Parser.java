20aa7e85700978301b8197411f607ac9e1a723b2
Khushi Singh <ksingh.172005@gmail.com>
2025-08-21 15:08:42 +0530

    feat: code unary(recursive descent/ top-down parser)

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -47,7 +47,20 @@ class Parser{
             Expr right = factor();
             expr = new Expr.Binary(expr, operator, right);
         }
+        return expr;
     }
+
+    private Expr factor(){
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)){
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
 
     private boolean match(TokenType...types){
         for(TokenType type : types){
