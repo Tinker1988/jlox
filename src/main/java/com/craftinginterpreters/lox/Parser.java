15f4c10c2dfb18e8de7f56a395a2209379f0e464
Khushi Singh <ksingh.172005@gmail.com>
2025-08-21 15:01:17 +0530

    feat: code equality(recursive descent/ top-down parser)

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -2,6 +2,8 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
+import com.craftinginterpreters.lox.TokenType.*;
+
 class Parser{
     private final List<Token> tokens;
 
@@ -10,6 +12,59 @@ class Parser{
     public Parser(List<Token> tokens) {
         this.tokens = null;
     }
+
+    private Expr expression(){
+        return equality();
+    }
+    
+    private Expr equality(){
+        Expr expr = comparison();
+        while (match(BANG_EQUAL, EQUAL_EQUAL)){
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+
+        }
+       
+       return expr;
+    }
+
+
+    private boolean match(TokenType...types){
+        for(TokenType type : types){
+            if(check(type)){
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type){
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance(){
+        if(!isAtEnd()) current++;
+        return previous();
+    }
+    private boolean isAtEnd(){
+        return peek().type == EOF;
+
+    }
+
+    private Token peek(){
+        return tokens.get(current);
+
+    }
+
+    private Token previous(){
+        return tokens.get(current - 1);
+    }
+
+
+
 
     
 }
