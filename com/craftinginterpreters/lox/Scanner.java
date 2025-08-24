444557e31d42feb739788e87c93c27cbce4da3da
Khushi Singh <ksingh.172005@gmail.com>
2025-08-24 17:34:17 +0530

    fix: typo and error causing identifier fixed

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -120,11 +120,11 @@ class Scanner {
     private void identifier() {
         while (isAlphaNumeric(peek())) advance();
 
-         String text = source.substring(start, current);
+        String text = source.substring(start, current);
         TokenType type = keywords.get(text);
         if (type == null) type = IDENTIFIER;
         addToken(type);
-        addToken(IDENTIFIER);
+        //addToken(IDENTIFIER);
 
     }
 
