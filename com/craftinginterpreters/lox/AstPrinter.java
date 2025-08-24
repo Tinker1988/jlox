82a742ec8a2ae5ade22a75b328395471769ba3ed
Khushi Singh <ksingh.172005@gmail.com>
2025-08-24 23:42:22 +0530

    feat : support for new expressions

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -7,6 +7,12 @@ class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr){
         return expr.accept(this);
     }
+
+    @Override
+public String visitAssignExpr(Expr.Assign expr) {
+    return parenthesize("= " + expr.name.lexeme, expr.value);
+}
+
     @Override
     public String visitBinaryExpr( Expr.Binary expr){
         return parenthesize(expr.operator.lexeme, expr.left,expr.right);
