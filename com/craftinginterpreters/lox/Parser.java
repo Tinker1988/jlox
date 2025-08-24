9286f7b0079c279d163ae77bb3b7a1a669d40645
Khushi Singh <ksingh.172005@gmail.com>
2025-08-24 18:40:48 +0530

    feat: generated file for variable

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -10,7 +10,7 @@ class Parser {
     List<Stmt> parse() {
         List<Stmt> statements = new ArrayList<>();
         while (!isAtEnd()) {
-            statements.add(statement());
+            statements.add(declaration());
         }
         return statements;
     }
@@ -30,6 +30,17 @@ class Parser {
         return equality();
     }
 
+    private Stmt declaration(){
+        try {
+           if(match(VAR)) return varDeclaration(); 
+
+           return statement();
+        } catch ( ParseError e) {
+            synchronize();
+            return null;
+        }
+    }
+
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
         
@@ -41,6 +52,17 @@ class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt varDeclaration() {
+        Token name = consume (IDENTIFIER, "Expect variable name.");
+        Expr initializer = null;
+        if(match(EQUAL)){
+            initializer = expression();
+        }
+
+        consume (SEMICOLON, "Expect ';' after variable declaration.");
+        return new Stmt.Var(name, initializer);
     }
 
     private Stmt expressionStatement() {
@@ -118,6 +140,10 @@ class Parser {
         if (match(TokenType.NUMBER, TokenType.STRING)) {
             return new Expr.Literal(previous().literal);
 
+        }
+
+        if(match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
         }
 
         if (match(LEFT_PAREN)) {
