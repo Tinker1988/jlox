5fb4f839b5f23d5f566d0920987aae96c1804c75
Khushi Singh <ksingh.172005@gmail.com>
2025-09-03 15:36:30 +0530

    feat: function to implement call

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,7 @@
 package com.craftinginterpreters.lox;
 
 import java.util.List;
+import java.util.ArrayList;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
 
@@ -223,6 +224,28 @@ public Object visitLogicalExpr(Expr.Logical expr) {
         //unreachable
         return null;
     }
+
+    @Override
+public Object visitCallExpr(Expr.Call expr) {
+    Object calle = evaluate(expr.callee);
+
+    List<Object> arguments = new ArrayList<>();
+    for(Expr argument : expr.arguments) {
+        arguments.add(evaluate(argument));
+    }
+
+    if (!(callee instanceof LoxCallable)) {
+        throw new RuntimeError(expr.paren, "Can only call funtions and classes.");
+    }
+
+    LoxCallable function = (LoxCallable)callee;
+    if(arguments.size() != function.arity()){
+        throw new RuntimeError(expr.paren, "Expected " + function.arity() + "arguments but got " + arguments.size() + ".");
+
+    }
+    return function.call(this, arguments);
+
+}
 }
 
 
